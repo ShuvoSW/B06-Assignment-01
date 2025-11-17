d30cb353a696162c6aa8253a85826d0f2c92b686
ShuvoSW <shuvomajumder3369@gmail.com>
2025-11-17 18:05:05 +0600

    Interface assign

--- solution.ts
+++ solution.ts
@@ -34,7 +34,7 @@ class Person {
     }
 
     getDetails() {
-        return (`Name: ${this.name}, Age: ${this.age}`)
+        return (`'Name: ${this.name}, Age: ${this.age}'`)
     }
 }
 
@@ -46,22 +46,85 @@ person2.getDetails();
 
 
 
-const filterByRating = (arrOfObj: object) => {
+// const filterByRating = (users: object[]) => {
 
-    Map.forEach(arrOfObj => {
-        console.log
-    });
-    // return arrOfObj.object.split("")
-    // let arr = 0
-    // if (arrOfItems.number <= 4) {
-    //     return arr.push[...arrOfItems]
-    // }
+//     users.filter(i => { 
+//         // console.log(i.rating )
+//         // if (i.rating <= 4) {
+//         //     return arr=push[i.rating];
+//         // }
+   
+//     });
+//     // return arrOfObj.object.split("")
+//     // let arr = 0
+//     // if (arrOfItems.number <= 4) {
+//     //     return arr.push[...arrOfItems]
+//     // }
+// }
+
+// const books = [
+//   { title: 'Book A', rating: 4.5 },
+//   { title: 'Book B', rating: 3.2 },
+//   { title: 'Book C', rating: 5.0 },
+// ];
+
+// console.log(filterByRating(books));
+
+
+
+// const filterActiveUsers = (user: object[]) => {
+
+//     // user.forEach(element => {
+//     //     console.log(element)
+//     // });
+// //     user.forEach(( key, value) =>
+// //         //  console.log("Key: ", key, "Value: ", value)
+// //     if (value.split === true)
+    
+// // );
+//    let arr = 0
+//     user.filter((i) =>  
+//         // console.log(i.isActive)
+//         if (i.isActive === "True") {
+//             return i.push = arr
+//         }
+// )
+
+
+// }
+
+// const users = [
+//   { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
+//   { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
+//   { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
+// ];
+
+// console.log(filterActiveUsers(users));
+
+// const topElectronicProducts = rawApiData.filter((item) => item.category == "Electronics")
+// .sort((a, b) => b.rating -a.rating)
+// .slice(0, 3)
+// .map((item) => ({name: item.productName}
+
+
+
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+    isAvailable: boolean;
 }
 
-const books = [
-  { title: 'Book A', rating: 4.5 },
-  { title: 'Book B', rating: 3.2 },
-  { title: 'Book C', rating: 5.0 },
-];
+const printBookDetails = (user: Book) => {
 
-console.log(filterByRating(books));
+    
+}
+
+const myBook: Book = {
+  title: 'The Great Gatsby',
+  author: 'F. Scott Fitzgerald',
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+console.log(printBookDetails(myBook))
